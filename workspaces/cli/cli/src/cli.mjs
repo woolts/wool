2d63c66c4192f3ba83545765c049a99b2c38550b
Laurence Roberts <lsjroberts@outlook.com>
2018-09-12 22:44:23 +0100

    Remove rollup

--- workspaces/cli/cli/src/cli.mjs
+++ workspaces/cli/cli/src/cli.mjs
@@ -4,20 +4,13 @@ import add from './commands/add';
 import bundle from './commands/bundle';
 import list from './commands/list';
 import local from './commands/local';
-// import make from './commands/make';
+import make from './commands/make';
 import version from './commands/version';
 
 const app = {
   name: 'wool',
   version: '0.0.0',
-  commands: [
-    add,
-    bundle,
-    list,
-    local,
-    // make,
-    version,
-  ],
+  commands: [add, bundle, list, local, make, version],
 };
 
 run(app, process.argv.slice(2)).catch(err => {
--- workspaces/cli/cli/src/commands/make/make.mjs
+++ workspaces/cli/cli/src/commands/make/make.mjs
@@ -1,25 +1,3 @@
-import { rollup } from 'rollup';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-
-import woolPlugin from './rollup-plugin';
-
 export default async function make({ args, options }) {
-  const bundle = await rollup({
-    input: args.path,
-    output: {
-      file: options.output,
-      format: 'cjs',
-    },
-    external: ['child_process', 'fs', 'path', 'readline', 'util'],
-    plugins: [
-      resolve(),
-      commonjs(),
-      woolPlugin(),
-      terser({ ecma: 8, toplevel: true }),
-    ],
-  });
-
-  await bundle.write({});
+  console.log('boo');
 }
