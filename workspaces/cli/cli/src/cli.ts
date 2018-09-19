36e94275b92758fd22d7c0992cb4f6cd2ad6da6e
Laurence Roberts <lsjroberts@outlook.com>
2018-09-19 08:59:14 +0100

    Disable todo commands

--- workspaces/cli/cli/src/cli.ts
+++ workspaces/cli/cli/src/cli.ts
@@ -1,16 +1,23 @@
 import run from 'wool/cli-args';
 
-import add from './commands/add';
-import bundle from './commands/bundle';
+// import add from './commands/add';
+// import bundle from './commands/bundle';
 import list from './commands/list';
-import local from './commands/local';
+// import local from './commands/local';
 import make from './commands/make';
-import version from './commands/version';
+// import version from './commands/version';
 
 const app = {
   name: 'wool',
   version: '0.0.0',
-  commands: [add, bundle, list, local, make, version],
+  commands: [
+    // add,
+    // bundle,
+    list,
+    // local,
+    make,
+    // version,
+  ],
 };
 
 run(app, process.argv.slice(2)).catch(err => {
